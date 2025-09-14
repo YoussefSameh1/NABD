4c70608328fdb6fda77726cb417f124617f99cc9
Youssef <ys1465@fayoum.edu.eg>
2025-09-14 12:27:43 +0300

    create the project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NabdApp());
+}
+
+class NabdApp extends StatelessWidget {
+  const NabdApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
